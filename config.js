775b5c2038b2c291cc0a9941b92911311b21ff28
Andr3wRiv3rs <andrewarivers@gmail.com>
2020-09-16 22:35:04 -0700

    adjust meta thumbnail

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: 'Andres Sweeney-Rios',
     description: 'Freelance frontend web specialist and indie game developer for hire. Feel free to contact me with any offers, feedback, or questions.',
     themeColor: '#ff69b4',
-    thumbnail: 'thumb.png'
+    thumbnail: 'https://poisonapple.dev/thumb.png'
   },
 
   port: 3000
